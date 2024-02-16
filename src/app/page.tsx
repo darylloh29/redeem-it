9d11abaa6c81ed9a0e3b818325ac3952f16a5a98
Daryl Loh <62453290+darylloh29@users.noreply.github.com>
2024-02-16 18:27:34 +0800

    Add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ type Staff = {
 
 type StaffList = Staff[];
 
-const TABLE_HEAD = ["Staff ID", "Team Name", "Status"];
+const TABLE_HEAD = ["Staff ID", "Team Name"];
 
 export default function Home() {
   const [search, setSearch] = useState<string>("");
@@ -48,6 +48,25 @@ export default function Home() {
 
   return (
     <main className="relative flex min-h-screen flex-col items-center p-24">
+      <div className="flex">
+        <h1 className="font-serif mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white p-4">
+          Gift Redemption
+        </h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-24 h-24 px-4"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+          />
+        </svg>
+      </div>
       <div className="relative z-10 max-w-5xl w-full items-center font-mono text-sm lg:flex">
         <div className="absolute w-full inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
           <svg
@@ -73,43 +92,43 @@ export default function Home() {
           onChange={handleSearchChange}
         />
       </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <table className="overflow-x-scroll w-full min-w-max table-auto text-left">
-          <thead>
-            <tr>
-              {TABLE_HEAD.map((head) => (
-                <th key={head} className="border-b border-black p-4">
-                  {head}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          {!displayNoResult && (
-            <tbody>
-              {displayList.length != 0
-                ? displayList.map(({ staff_pass_id, team_name }, index) => {
-                    const classes = "p-4";
-                    return (
-                      <tr key={index}>
-                        <td className={classes}>{staff_pass_id}</td>
-                        <td className={classes}>{team_name}</td>
-                        <td className={classes}>Not Redeemed</td>
-                      </tr>
-                    );
-                  })
-                : staffList.map(({ staff_pass_id, team_name }, index) => {
-                    const classes = "p-4";
-                    return (
-                      <tr key={index}>
-                        <td className={classes}>{staff_pass_id}</td>
-                        <td className={classes}>{team_name}</td>
-                        <td className={classes}>Not Redeemed</td>
-                      </tr>
-                    );
-                  })}
-            </tbody>
-          )}
-        </table>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex max-h-full">
+        <div className="overflow-y-auto max-h-96 w-full">
+          <table className="w-full min-w-max table-auto text-left">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th key={head} className="border-b border-black p-4">
+                    {head}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            {!displayNoResult && (
+              <tbody>
+                {displayList.length != 0
+                  ? displayList.map(({ staff_pass_id, team_name }, index) => {
+                      const classes = "p-4";
+                      return (
+                        <tr key={index}>
+                          <td className={classes}>{staff_pass_id}</td>
+                          <td className={classes}>{team_name}</td>
+                        </tr>
+                      );
+                    })
+                  : staffList.map(({ staff_pass_id, team_name }, index) => {
+                      const classes = "p-4";
+                      return (
+                        <tr key={index}>
+                          <td className={classes}>{staff_pass_id}</td>
+                          <td className={classes}>{team_name}</td>
+                        </tr>
+                      );
+                    })}
+              </tbody>
+            )}
+          </table>
+        </div>
       </div>
       {displayNoResult && (
         <div className="p-4">
