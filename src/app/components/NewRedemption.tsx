5d29c4bde31ef7176bed4205b85d4f77a530f15e
Daryl Loh <62453290+darylloh29@users.noreply.github.com>
2024-02-17 17:06:06 +0800

    Change button type to submit

--- src/app/components/NewRedemption.tsx
+++ src/app/components/NewRedemption.tsx
@@ -27,8 +27,10 @@ export default function NewRedemption(props: NewRedemptionProps) {
     setStaffID(event.target.value);
   };
 
-  const handleRedeemClick = () => {
-    console.log(staffList);
+  const handleSubmit = () => {
+    if (staffID.length < 1) {
+      return;
+    }
     console.log(staffID);
   };
 
@@ -37,7 +39,13 @@ export default function NewRedemption(props: NewRedemptionProps) {
       <h1 className="text-3xl text-gray-900 md:text-3xl lg:text-4xl dark:text-white p-4">
         New Redemption
       </h1>
-      <form className="flex rounded p-4">
+      <form
+        className="flex rounded p-4"
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSubmit();
+        }}
+      >
         <input
           className="shadow appearance-none border rounded w-full mr-2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="Staff ID"
@@ -47,8 +55,7 @@ export default function NewRedemption(props: NewRedemptionProps) {
         />
         <button
           className="bg-gray-500 hover:bg-gray-700 text-white rounded focus:outline-none focus:shadow-outline py-2 px-4"
-          type="button"
-          onClick={handleRedeemClick}
+          type="submit"
         >
           Redeem
         </button>
